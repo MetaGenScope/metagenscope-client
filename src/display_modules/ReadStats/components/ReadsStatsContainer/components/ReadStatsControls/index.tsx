f4834edbbfe3d516563dda87c7a8680156f49380
David Danko <dcdanko@gmail.com>
2018-04-13 06:25:51 -0400

    read stats (probably) complete as a column plot

---
+++ src/display_modules/ReadStats/components/ReadsStatsContainer/components/ReadStatsControls/index.tsx
@@ -0,0 +1,36 @@
+import * as React from 'react';
+import * as d3 from 'd3';
+import { Well, Row, Col } from 'react-bootstrap';
+
+import SelectSource from '../../../../../controls/SelectSource';
+
+type ControlsProps = {
+  sources: string[];
+  activeSource: string;
+  handleSourceChange(newSource: string): void;
+};
+
+class ReadStatsControls extends React.Component<ControlsProps, {}> {
+  constructor(props: ControlsProps) {
+    super(props);
+    this.handleSourceChange = this.handleSourceChange.bind(this);
+  }
+
+  handleSourceChange(newSource: string) {
+    this.props.handleSourceChange(newSource);
+  }
+
+  render() {
+    return (
+      <Well>
+        <Row>
+          <Col lg={12} md={4} sm={12}>
+            <SelectSource {...this.props} />
+          </Col>
+        </Row>
+      </Well>
+    );
+  }
+}
+
+export default ReadStatsControls;
--- src/display_modules/ReadStats/components/ReadsStatsContainer/index.tsx
+++ src/display_modules/ReadStats/components/ReadsStatsContainer/index.tsx
@@ -1,69 +1,114 @@
 import * as React from 'react';
 import * as Highcharts from 'highcharts';
+import * as d3 from 'd3';
+import { Row, Col } from 'react-bootstrap';
 
+import HighChartsPlot from '../../../plots/HighChartsPlot';
+import { ReadStatsResultType } from '../../../../services/api/models/queryResult';
 import { ChartRefProps } from '../../../components/DisplayContainer/highcharts';
-import { ReadStatsType } from '../../../../services/api/models/queryResult';
 
-const ReadStatsContainer: React.SFC<{data: ReadStatsType} & ChartRefProps> = (props) => {
+import ReadStatsControls from './components/ReadStatsControls';
 
+export interface ReadStatsProps extends ChartRefProps {
+  data: ReadStatsResultType;
+}
 
-  const series: Highcharts.IndividualSeriesOptions[] = props.data.data.map(source => {
-    return {
-      name: source.category,
-      data: source.values,
+export interface ReadStatsState {
+  activeSource: string;
+}
+
+export class ReadStatsContainer extends React.Component<ReadStatsProps, ReadStatsState> {
+
+  protected color: d3.ScaleOrdinal<string, string>;
+
+  constructor(props: ReadStatsProps) {
+    super(props);
+
+    this.handleSourceChange = this.handleSourceChange.bind(this);
+
+    this.state = {
+      activeSource: 'num_reads',
     };
-  });
+  }
 
-  const chartOptions: Highcharts.Options = {
-    chart: {
-      type: 'column',
-    },
-    title: {
-      text: null,
-    },
-    xAxis: {
-      categories: props.data.sample_names,
-    },
-    yAxis: {
-      min: 0,
-      max: 100,
+  handleSourceChange(source: string) {
+    this.setState({
+      activeSource: source,
+    });
+  }
+
+  chartOptions(activeSource: string): Highcharts.Options {
+
+    let valList: Array<[string, number]> = [];
+    for (let sname in this.props.data.samples) {
+      val = this.props.data.samples[sname][activeSource];
+      valList.push([sname, val]);
+    }
+    let sortedVals: Array<[string, number]> = valList.sort((el0, el1) => el0[1] - el1[1]);
+    const sampleNames = sortedVals.map(el => el[0]);
+    const dataPoints = sortedVals.map(el => el[1]);
+
+    if (activeSource == 'num_reads') {
+      const yAxisTitle = 'Num. Reads';
+    } else if (activeSource == 'gc_content'){
+      const yAxisTitle = 'GC Percentage';
+    }
+
+
+    const chartOptions: Highcharts.Options = {
+      chart: {
+        type: 'column',
+      },
       title: {
-        text: 'Proportion of Reads Classified',
+        text: null,
       },
-      stackLabels: {
+      legend: {
+        enabled: true,
+      },
+      xAxis: {
+        categories: sampleNames,
+      },
+      yAxis: {
+        title: {
+          text: yAxisTitle,
+        },
+      },
+      exporting: {
         enabled: false,
-        style: {
-          fontWeight: 'bold',
-          color: 'gray',
-        },
       },
-    },
-    tooltip: {
-      headerFormat: '<b>{point.x}</b><br/>',
-      pointFormat: '{series.name}: {point.y}<br/>Total: {point.stackTotal}',
-    },
-    plotOptions: {
-      column: {
-        stacking: 'normal',
-        dataLabels: {
-          enabled: false,
-          color: 'white',
-        },
-      },
-    },
-    exporting: {
-      enabled: false,
-    },
-    series,
-  };
+      series: [{
+        name: 'Sample',
+        data: dataPoints,
+      }],
+    };
 
-  return (
-    <HighChartsPlot
-      chartId="read-stats"
-      options={chartOptions}
-      chartRef={props.chartRef}
-    />
-  );
-};
+    return chartOptions;
+  }
+
+  render() {
+    const activeSource = this.state.activeSource;
+    const chartOptions = this.chartOptions(activeSource);
+
+    return (
+      <Row>
+        <Col lg={9}>
+          <HighChartsPlot
+            chartId="read-stats"
+            options={chartOptions}
+            chartRef={this.props.chartRef}
+          />
+        </Col>
+        <Col lg={3}>
+          <ReadStatsControls
+            sources={['num_reads', 'gc_content']}
+            activeSource={activeSource}
+            handleSourceChange={this.handleSourceChange}
+          />
+        </Col>
+      </Row>
+    );
+  }
+
+}
 
 export default ReadStatsContainer;
