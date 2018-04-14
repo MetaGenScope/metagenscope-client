e6fa2dea905ae1182f9284f00fa134bc8d27f720
David Danko <dcdanko@gmail.com>
2018-04-14 15:49:30 -0400

    added VFDB gene module based on Methyl DisplayModule

---
+++ src/display_modules/VFDB/components/VFDBContainer/components/VFDBControls/index.tsx
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import { Well } from 'react-bootstrap';
+
+import DropdownSelect from '../../../../../controls/DropdownSelect';
+
+type ControlsProps = {
+  metrics: string[];
+  activeMetric: string;
+  handleMetricChange(metric?: string): void;
+};
+
+const VFDBControls: React.SFC<ControlsProps> = (props) => {
+  return (
+    <Well>
+      <DropdownSelect
+        label="Metric"
+        options={props.metrics}
+        activeOption={props.activeMetric}
+        handleOptionChange={props.handleMetricChange}
+      />
+    </Well>
+  );
+};
+
+export default VFDBControls;
---
+++ src/display_modules/VFDB/index.tsx
@@ -0,0 +1,30 @@
+import * as React from 'react';
+
+import D3DisplayContainer from '../components/DisplayContainer/d3';
+import { DisplayContainerProps } from '../components/DisplayContainer';
+import { VFDBType } from '../../services/api/models/queryResult';
+import { getVFDB } from '../../services/api';
+
+import VFDBContainer from './components/VFDBContainer';
+
+export default class VFDBModule extends D3DisplayContainer<VFDBType> {
+
+  constructor(props: DisplayContainerProps) {
+    super(props);
+
+    this.title = 'VFDBTransferase Abundance';
+    this.description = (
+      <p>The abundance of VFDBtransferase genes.</p>
+    );
+  }
+
+  /** @inheritdoc */
+  fetchData() {
+    return getVFDB(this.props.uuid);
+  }
+
+  /** @inheritdoc */
+  plotContainer(data: VFDBType): JSX.Element {
+    return <VFDBContainer data={data} svgRef={el => this.svgCanvas = el} />;
+  }
+}
