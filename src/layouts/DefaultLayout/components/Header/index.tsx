14467e77e26eeb2d61be4b161b2a08d2fc55273c
Benjamin Chrobot <benjamin.chrobot@alum.mit.edu>
2018-04-19 15:47:09 -0400

    Make worldquant theme full-bleed and remove default navigation.

--- src/layouts/DefaultLayout/components/Header/index.tsx
+++ src/layouts/DefaultLayout/components/Header/index.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Row, Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
+import { Grid, Row, Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 import { Link } from 'react-router-dom';
 
@@ -11,69 +11,71 @@ interface HeaderProps {
 class Header extends React.Component<HeaderProps, {}> {
   render() {
     return (
-      <Row>
-        <Navbar collapseOnSelect={true}>
-          <Navbar.Header>
-            <Navbar.Brand>
-              <Link to="/">{this.props.title}</Link>
-            </Navbar.Brand>
-            <Navbar.Toggle />
-          </Navbar.Header>
-          <Navbar.Collapse>
-            <Nav pullRight={true}>
-              <LinkContainer to="/about">
-                <NavItem>About</NavItem>
-              </LinkContainer>
-              <LinkContainer to="/docs">
-                <NavItem>Documentation</NavItem>
-              </LinkContainer>
-              <LinkContainer to="/organizations">
-                <NavItem>Organizations</NavItem>
-              </LinkContainer>
-              {this.props.isAuthenticated &&
-                <NavDropdown title="+" id="basic-nav-dropdown">
-                  <LinkContainer to="#">
-                    <MenuItem>Project</MenuItem>
-                  </LinkContainer>
-                  <LinkContainer to="/organizations/new">
-                    <MenuItem>Organization</MenuItem>
-                  </LinkContainer>
+      <Grid>
+        <Row>
+          <Navbar collapseOnSelect={true}>
+            <Navbar.Header>
+              <Navbar.Brand>
+                <Link to="/">{this.props.title}</Link>
+              </Navbar.Brand>
+              <Navbar.Toggle />
+            </Navbar.Header>
+            <Navbar.Collapse>
+              <Nav pullRight={true}>
+                <LinkContainer to="/about">
+                  <NavItem>About</NavItem>
+                </LinkContainer>
+                <LinkContainer to="/docs">
+                  <NavItem>Documentation</NavItem>
+                </LinkContainer>
+                <LinkContainer to="/organizations">
+                  <NavItem>Organizations</NavItem>
+                </LinkContainer>
+                {this.props.isAuthenticated &&
+                  <NavDropdown title="+" id="basic-nav-dropdown">
+                    <LinkContainer to="#">
+                      <MenuItem>Project</MenuItem>
+                    </LinkContainer>
+                    <LinkContainer to="/organizations/new">
+                      <MenuItem>Organization</MenuItem>
+                    </LinkContainer>
+                  </NavDropdown>
+                }
+                <NavDropdown title="Account" id="basic-nav-dropdown">
+                  {!this.props.isAuthenticated &&
+                    <LinkContainer to="/register">
+                      <MenuItem>Register</MenuItem>
+                    </LinkContainer>
+                  }
+                  {!this.props.isAuthenticated &&
+                    <LinkContainer to="/login">
+                      <MenuItem>Log In</MenuItem>
+                    </LinkContainer>
+                  }
+                  {this.props.isAuthenticated &&
+                    <LinkContainer to="/status">
+                      <MenuItem>User Status</MenuItem>
+                    </LinkContainer>
+                  }
+                  {this.props.isAuthenticated &&
+                    <LinkContainer to="/dashboard">
+                      <MenuItem>Dashboard</MenuItem>
+                    </LinkContainer>
+                  }
+                  {this.props.isAuthenticated &&
+                    <MenuItem divider={true} />
+                  }
+                  {this.props.isAuthenticated &&
+                    <LinkContainer to="/logout">
+                      <MenuItem>Log Out</MenuItem>
+                    </LinkContainer>
+                  }
                 </NavDropdown>
-              }
-              <NavDropdown title="Account" id="basic-nav-dropdown">
-                {!this.props.isAuthenticated &&
-                  <LinkContainer to="/register">
-                    <MenuItem>Register</MenuItem>
-                  </LinkContainer>
-                }
-                {!this.props.isAuthenticated &&
-                  <LinkContainer to="/login">
-                    <MenuItem>Log In</MenuItem>
-                  </LinkContainer>
-                }
-                {this.props.isAuthenticated &&
-                  <LinkContainer to="/status">
-                    <MenuItem>User Status</MenuItem>
-                  </LinkContainer>
-                }
-                {this.props.isAuthenticated &&
-                  <LinkContainer to="/dashboard">
-                    <MenuItem>Dashboard</MenuItem>
-                  </LinkContainer>
-                }
-                {this.props.isAuthenticated &&
-                  <MenuItem divider={true} />
-                }
-                {this.props.isAuthenticated &&
-                  <LinkContainer to="/logout">
-                    <MenuItem>Log Out</MenuItem>
-                  </LinkContainer>
-                }
-              </NavDropdown>
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
-      </Row>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+        </Row>
+      </Grid>
     );
   }
 }
