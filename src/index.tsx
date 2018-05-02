1bf274ef35ff2002da452e94f345496c8ebf32bd
Benjamin Chrobot <benjamin.chrobot@alum.mit.edu>
2018-05-02 08:25:15 -0400

    Clean up utilitiy declarations.

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 
 import App from './App';
 import { unregister } from './registerServiceWorker';
+import './utils';
 import './index.css';
 
 ReactDOM.render(
