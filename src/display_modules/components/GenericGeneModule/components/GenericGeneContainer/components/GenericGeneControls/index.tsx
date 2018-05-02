77e3e12b2bbc16e3b4541041a102083da2745251
David Danko <dcdanko@gmail.com>
2018-05-02 10:16:28 -0400

    treat rpkm special

--- src/display_modules/components/GenericGeneModule/components/GenericGeneContainer/components/GenericGeneControls/index.tsx
+++ src/display_modules/components/GenericGeneModule/components/GenericGeneContainer/components/GenericGeneControls/index.tsx
@@ -3,6 +3,13 @@ import { Well } from 'react-bootstrap';
 
 import DropdownSelect from '../../../../../../controls/DropdownSelect';
 
+const handleCase = (str: string) => {
+  if('rpkm' in str.toLowerCase()){
+    return str.toUpperCase();
+  }
+  return str;
+};
+
 type ControlsProps = {
   metrics: string[];
   activeMetric: string;
@@ -10,12 +17,14 @@ type ControlsProps = {
 };
 
 const GenericGeneControls: React.SFC<ControlsProps> = (props) => {
+  const metrics = props.metrics.map(handleCase),
+        activeMetric = handleCase(props.activeMetric);
   return (
     <Well>
       <DropdownSelect
         label="Metric"
-        options={props.metrics}
-        activeOption={props.activeMetric}
+        options={metrics}
+        activeOption={activeMetric}
         handleOptionChange={props.handleMetricChange}
       />
     </Well>
