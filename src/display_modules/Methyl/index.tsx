a971ede34699bc0c596f14ccbd86693f0963d95b
David Danko <dcdanko@gmail.com>
2018-05-02 11:01:06 -0400

    unsaved methyl

--- src/display_modules/Methyl/index.tsx
+++ src/display_modules/Methyl/index.tsx
@@ -16,7 +16,7 @@ export default class MethylModule extends GenericGeneModule {
          These genes are responsible for genetic regulation.</p>
       <p>Samples are compared directly to amino acid sequences. Displayed values {' '}
          the log10 of Reads per Kilobase per Millions (RPKM) or Reads per {' '}
-         Kilobase per Millions per Genome Size (RPKMG).</p>
+         Kilobase per Millions per Genome Base (RPKMG).</p>
     );
 
     this.chartId = 'methyl-chart';
